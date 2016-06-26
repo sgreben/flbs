1b36734732aecf4acd92fc531d510d9513b5c611
Sergey Grebenshchikov <sgreben@gmail.com>
2016-06-27 01:56:54 +0200

    Cosmetics

--- src/main/java/sgreben/flbs/StateTable.java
+++ src/main/java/sgreben/flbs/StateTable.java
@@ -1,21 +1,27 @@
 package sgreben.flbs;
 
 public interface StateTable {
+	
 	/** Number of states in this table
 	 */
 	public int size();
+	
 	/** The residuals of the given state
 	 */
-	public Residuals residuals(int state);
-	/** Returns, or creates and returns, the state with the given residuals 
+	public Residuals get(int state);
+	
+	/** The residual of the given state w.r.t. the given symbol.
 	 */
 	public int make(Residuals residuals);
+	
 	/** True iff a state with the given residuals exists
 	 */
 	public boolean exists(Residuals residuals);
+	
 	/** The state representing the empty set 
 	 */
 	public int ZERO();
+	
 	/** The state representing the language containing only the empty word.
 	 */
 	public int EPSILON();
